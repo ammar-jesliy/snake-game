3d55d040867ffefad91b96f82250667ade518e4d
annar-jesliy <ammarjc1@gmail.com>
2024-04-24 11:46:57 +0530

    Created snake and movement functions

--- snake.js
+++ snake.js
@@ -1,9 +1,92 @@
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d");
 
+let snake = [ {x: 150, y: 150}, {x: 140, y: 150}, {x: 130, y: 150}, {x: 120, y: 150}, {x: 110, y: 150} ];
+
+let dx = 10;
+let dy = 0;
+
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, 300, 300);
 
 ctx.strokeStyle = "black";
-ctx.lineWidth = 5;
+ctx.lineWidth = 2;
 ctx.strokeRect(0, 0, 300, 300);
+
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokeStyle = 'darkgreen';
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.lineWidth = 2
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function changeDirection(event) {
+    const LEFT_KEY = "ArrowLeft";
+    const RIGHT_KEY = "ArrowRight";
+    const UP_KEY = "ArrowUp";
+    const DOWN_KEY = "ArrowDown";
+
+    const keyPressed = event.key;
+
+    console.log(keyPressed);
+
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10;
+        dy = 0;
+    }
+
+    if (keyPressed === UP_KEY && !goingDown) {
+        dx = 0;
+        dy = -10;
+    }
+
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10;
+        dy = 0;
+    }
+
+    if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0;
+        dy = 10;
+    }
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
+
+function advanceSnake() {
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    snake.unshift(head);
+    snake.pop();
+}
+
+function clearCanvas() {
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+
+    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
+
+function main() {
+    setTimeout(function onTick() {
+        clearCanvas();
+        advanceSnake();
+        drawSnake();
+        main();
+    }, 100);
+
+    document.addEventListener("keydown", changeDirection);
+}
+
+main()
+
+
+drawSnake();
